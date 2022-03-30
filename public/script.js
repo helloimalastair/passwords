c8c0f2342d0e18d58f5e2473120a2154ea9e94f2
Alastair Rosewood <37487746+helloimalastair@users.noreply.github.com>
2022-03-30 10:51:52 -0700

    add logging

--- public/script.js
+++ public/script.js
@@ -1,22 +1,45 @@
 const myWorker = new Worker("worker.js"),
 	output = document.getElementById("output");
+let mouseData = "",
+  logging = false;
+
+document.addEventListener("mousemove", e => {
+  const s = (e.clientX+e.clientY).toString(16);
+  if(mouseData.length >= 100000) mouseData = mouseData.substring(s.length);
+  mouseData += s;
+});
 
 myWorker.onmessage = e => {
 	const data = e.data;
 	if(data.error) console.error(e.data.error);
-	if(data.password) output.innerText = data.password;
-	if(data.passwords) output.innerText = JSON.stringify(data.passwords);
-	console.log(`Operation took ${e.data.performance} ms.`)
+	if(Array.isArray(data))output.innerText = JSON.stringify(data);
+  else output.innerText = data;
 }
 
 const getData = () => {
   return {
+    logging,
     num: document.getElementsByName("num")[0].checked,
     alpha: document.getElementsByName("alpha")[0].checked,
     char: document.getElementsByName("char")[0].checked,
     len: document.getElementsByName("len")[0].value,
-    seed: document.getElementsByName("seed")[0].value,
+    suppliedSeed: document.getElementsByName("seed")[0].value,
     amt: document.getElementsByName("amt")[0].value,
-    randOrg: document.getElementsByName("randOrg")[0].checked
+    randOrg: document.getElementsByName("randOrg")[0].checked,
+    mouseData
   };
+};
+
+console.log("AlastairOS Browser Edition v.1.7.8 Ready.");
+console.log("Worker Loaded.");
+console.log("Mouse Logging Enabled.");
+
+const logMode = () => {
+  if(logging) {
+    logging = false;
+    return "Logging is now off.";
+  } else {
+    logging = true;
+    return "Logging is now on.";
+  }
 };
